436e77949c0b38bdd4db97935b46a2ec1f97faa1
Harpreet  Janjua <harpreetjanjua@Harpreets-MacBook-Pro.local>
2022-02-08 12:23:58 -0800

    first commit

---
+++ lecture.js
@@ -0,0 +1,37 @@
+// var beatles = ["Paul", "George", "John", "Rings"]
+
+// function printNames(arr){
+//     console.log("Inside of printNames(")
+//     var example ="hello"
+
+//     function actuallyPrintingNames(){
+//         console.log(example);
+//         for (var index = 0; index < arr.length; index++)
+//         var member = arr[index];
+//         console.log(member + "was found at index" + index);
+//     }
+//     console.log(beatles);
+//     actuallyPrintingNames();
+
+// }
+
+// printNames(beatles);
+
+
+// ES6 var scoping/ 
+// Block scoping helps us determine where we want our scopes to be. 
+// Let and const allow for block scoping. 
+
+// const beatles = ["Paul", "George", "John", "Rings"];
+
+// function actuallyPrintingNames() {
+//     let index = 0;
+//     let name;
+//     for (index; index < names.length; index++) {
+//       name = names[index];
+//       console.log(name + ' was found at index ' + index);
+//     }
+//     console.log('name and index after loop is ' + name + ':' + index);
+//   }
+
+
