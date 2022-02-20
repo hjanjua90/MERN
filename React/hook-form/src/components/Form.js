fe42151a4cba147877a32cfa23f6f60772c9577f
hjanjua90 <harpreet_mann23@yahoo.com>
2022-02-20 14:06:39 -0800

    "hook-form"

---
+++ React/hook-form/src/components/Form.js
@@ -0,0 +1,117 @@
+import React, {useState} from 'react';
+
+const Form = (props) => {
+    const [firstName, setFirstName] =useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+
+    const formDataDivStyle ={
+        textAlign: "left",
+        width: "450px",
+        margin: "auto",
+    }
+
+
+    const inputDataDivStyle = {
+        borderRadius: "5px",
+        backgroundColor: "#f2f2f2",
+        border: "1px solid darkgrey",
+        padding: "0px 10px",
+        margin: "5px",
+    }
+
+
+
+
+
+
+    return (
+
+        <div>
+            <form style ={{marginTop: "20px"}}>
+                <div style={inputDataDivStyle}>
+                    <label>First Name</label>
+                    <input
+                    type="text"
+                    name="firstName"
+                    onChange={(e => setFirstName(e.target.value))}
+                    />
+                </div>
+                <div>
+                    <label>Last Name</label>
+                    <input
+                    type="text"
+                    name="lastName"
+                    onChange={(e) => setLastName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input
+                    type="text"
+                    name="email"
+                    onChange={(e)=>setEmail(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input
+                    type="password"
+                    name="password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <div>
+                <label>Confirm Password</label>
+                    <input
+                    type="password"
+                    name="confirmPassword"
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                </div>
+            </form>
+
+
+            <div style ={formDataDivStyle}>
+                <h3 style={{textAlign:'center'}}>Form Data</h3>
+                <p>
+                    <label>First Name: </label> { firstName}
+                </p>
+                <p>
+                    <label>Last Name: </label> {lastName}
+                </p>
+                <p>
+                    <label>Email: </label> {email}
+                </p>
+                <p>
+                    <label>Password: </label> {password}
+                </p>
+                <p>
+                    <label> Confirm Password</label> {confirmPassword}
+                </p>
+
+
+
+            </div>
+
+
+
+
+        </div>
+
+
+
+
+    )
+
+
+
+
+}
+
+
+
+export default Form; 
