23f01007974bfa3368d29789d5e8eff602a5b1f4
hjanjua90 <harpreet_mann23@yahoo.com>
2022-02-26 14:35:11 -0800

    box-generator

---
+++ React/box-generator/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import React, {useState} from 'react';
+import Form from './components/Form';
+import Display from './components/Display';
+
+
+function App() {
+  const [boxColorArray, setBoxColorArray] =useState([]);
+
+  return (
+    <div className="App">
+      <h1>Box-Generator</h1>
+      <Form boxColorArray={boxColorArray} setBoxColorArray={setBoxColorArray}/>
+      <Display boxColorArray={boxColorArray}/>
+    </div>
+  );
+}
+
+export default App;
