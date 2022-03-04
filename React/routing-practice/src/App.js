f8cc89482c75ed4989b83ecbc546965533f9c1b2
hjanjua90 <harpreet_mann23@yahoo.com>
2022-03-04 14:55:27 -0800

    routing-practice

---
+++ React/routing-practice/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import ParamsComponent from './components/ParamsComponent';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className='App'>
+      <Routes>
+        <Route element={<Home/>} path ="/home"/>
+        <Route element={<ParamsComponent/>} path="/:word"/>
+        <Route element={<ParamsComponent/>} path="/:word/:color/:bgCol"/>
+      </Routes>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ React/routing-practice/src/components/ParamsComponent.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {useParams} from "react-router-dom";
+
+const ParamsComponent = (props) =>{
+
+    const {word, color, bgCol} = useParams();
+
+
+
+
+
+    return(
+        <div>
+            
+            {
+                isNaN(word)?
+                <p style={
+                    color?
+                    
+                        {color: color, backgroudColor: bgCol}
+                        : null
+                    
+                }>
+                    This is a word : {word}
+                </p>
+                :
+                <p>
+                    This is a number : {word}
+                </p>
+            }
+
+        </div>
+    )
+}
+
+export default ParamsComponent;
